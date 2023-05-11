74e3c16ef374475227d5322935dea1c333dcbe7d
Joe Sandmeyer <github.com@unconscious.com>
2023-05-11 18:02:19 -0400

    renamed component to VideoQueueComponent

--- src/VideoQueueComponent.js
+++ src/VideoQueueComponent.js
@@ -1,30 +1,31 @@
 import { BaseComponent }  from 'vatom-spaces-plugins'
 
 
-export class VideoScreenComponent extends BaseComponent {
-//================ Properties of class VideoScreenComponent
+export class VideoQueueComponent extends BaseComponent {
+//================ Properties of class VideoQueueComponent
 
-  // Instance properties
-  myCenter = null;
+// Instance properties
+currentVideo = null;
+queuedVideo = null;
 
 
-//================ Begin methods for class VideoScreenComponent
+//================ Begin methods for class VideoQueueComponent
 
-  // Called when the component is loaded
-  async onLoad() {
-  }
+// Called when the component is loaded
+async onLoad() {
+}
 
-  // Called when the component is unloaded
-  onUnload() {
-  }// onUnload
+// Called when the component is unloaded
+onUnload() {
+}// onUnload
 
-  // Called when a remote message is received
-  onMessage(msg) {
-  }
+// Called when a remote message is received
+onMessage(msg) {
+}
 
-  onObjectUpdated(newFields){
-  }// onObjectUpdated()
+onObjectUpdated(newFields){
+}// onObjectUpdated()
 
-//================ End of methods for class VideoScreenComponent
-}// class VideoScreenComponent
+//================ End of methods for class VideoQueueComponent
+}// class VideoQueueComponent
 
